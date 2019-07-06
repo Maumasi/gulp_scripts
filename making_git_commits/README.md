e24f4742d84591b28c4e04964c58d11d691ddda1
Liu Maumasi <liu.maumasi@gmail.com>
2019-07-06 08:21:25 -0500

    worked on README file

---
+++ making_git_commits/README.md
@@ -0,0 +1,77 @@
+
+# Quickly add, commit with comment, and push
+---
+<b>
+
+## Requirements: NPM/NPX
+Command in the Terminal:
+```bash
+npm install gulp shelljs yargs --save
+```
+---
+<b>
+
+
+## Message types:
+
+Messages are formatted to to show which **BRANCH** changes were committed to. Which **USER** made the commit. The message type, **AUTO-COMMIT, MESSAGE, HOTFIX, FEATURE, STABLE,** or **BROKEN**.
+<b>
+
+To add a message to a commit, provide the **-m** option.
+<b>
+
+*A standard message commit*
+Commands in the Terminal:
+```bash
+gulp push -m "setting up gulp automation"
+
+# output
+# [BRANCH] master | [USER] Liu Maumasi :: MESSAGE :: setting up gulp automation
+```
+
+### Setting a specific commit header:
+Message options are:
+* **-s**: *STABLE*
+* **-b**: *BROKEN*
+* **-f**: *FEATURE*
+* **-x**: *HOTFIX*
+* *Note: if no options are provided an auto-commit __AUTO-COMMIT__ *message header will be provided.*
+
+
+*A standard message commit*
+Commands in the Terminal:
+```bash
+gulp push -m "setting up gulp automation"
+# output
+# [BRANCH] master | [USER] Liu Maumasi :: MESSAGE :: setting up gulp automation
+
+# STABLE with message
+gulp push -sm "setting up gulp automation"
+
+# output
+# [BRANCH] master | [USER] Liu Maumasi :: STABLE :: setting up gulp automation
+
+# BROKEN with message
+gulp push -bm "feature not working as expected"
+
+# output
+# [BRANCH] master | [USER] Liu Maumasi :: BROKEN :: feature not working as expected
+
+# AUTO-COMMIT
+gulp push
+
+# output
+# [BRANCH] master | [USER] Liu Maumasi :: AUTO-COMMIT :: no message for commit.
+
+# AUTO-COMMIT message with option header
+gulp push -s
+
+# output
+# [BRANCH] master | [USER] Liu Maumasi :: STABLE :: no message for commit.
+```
+---
+<b>
+
+## Generated message:
+
+The **[BRANCH]** and **[USER]** components of the message are auto generated and will be generated from the current active branch and the git user assigned when to the GIT instance on the machine.
---
+++ making_git_commits/gulpfile.js
@@ -0,0 +1,92 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const shell = require('shelljs');
+const { argv } = require('yargs');
+
+// git info
+const gitInfo = {
+  user: shell.exec('`which git` config --list | grep "user.name"').split('=')[1].split('\n')[0],
+  branch: shell.exec('`which git` branch | `which grep` "*"').stdout.split(' ')[1].split('\n')[0],
+}
+// paths
+// const watchPaths = {
+//   libSass: '_dev/sass_files/**/*.sass',
+//   mainSass: '_dev/sass_files/**/*.sass',
+// };
+
+
+// =================== SASS ====================================================
+
+// function compileSass(done) {
+//   return gulp.src('./_dev/sass_files/main.sass')
+//     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//     .pipe(gulp.dest('./client'));
+//     done();
+// }
+// // task
+// gulp.task('sass', compileSass);
+
+// =================== Watch  ==================================================
+// function watch(done) {
+//   const { libSass, mainSass } = watchPaths;
+//
+//   // all task to run before startinf the watch task
+//   gulp.series(compileSass);
+//   // add all watch tasks here
+//   gulp.watch(libSass, compileSass);
+//   gulp.watch(mainSass, compileSass);
+//   done();
+// }
+
+// task
+// gulp.task('watch', watch);
+
+// =================== Git  ====================================================
+
+function gitMessageBuilder() {
+  const { user, branch } = gitInfo;
+  let heading = '';
+  if(argv.s) {
+    heading = 'STABLE'
+  } else if(argv.b) {
+    heading = 'BROKEN'
+  } else if(argv.f) {
+    heading = 'FEATURE'
+  } else if(argv.x) {
+    heading = 'HOTFIX'
+  } else if(argv.m) {
+    heading = 'MESSAGE';
+  } else {
+    heading = 'AUTO-COMMIT';
+  }
+
+  let message = argv.m;
+
+  if(!message || !(message.length > 0)) {
+    message = 'no message for commit.';
+  }
+  return `[BRANCH] ${branch} | [USER] ${user} :: ${heading} :: ${message}`;
+}
+
+
+function commitAndPush(done) {
+  const { branch } = gitInfo;
+  let gitJob = `\`which git\` add .`;
+  gitJob += ` && \`which git\` commit --message "${gitMessageBuilder()}"`;
+  gitJob += ` && \`which git\` push -u github ${branch}`;
+  gitJob += ` && \`which git\` push -u heroku ${branch}`;
+  shell.exec(gitJob);
+  done();
+}
+
+
+gulp.task('push', gulp.series(compileSass, commitAndPush));
+
+// =================== Default =================================================
+// just type `gulp` in the terminal to execute all the gulp tasks
+gulp.task('default', gulp.series(compileSass, watch));
+
+
+
+
+//
